a3260a8d9ccb3f4d39542995a384a8eb7280e266
Mayhem-XD <leahblo1994@gmail.com>
2023-04-06 16:21:22 +0900

    모듈사용해서_만들기

--- 07.모듈/test_module.py
+++ 07.모듈/test_module.py
@@ -12,3 +12,8 @@ def get_area(radius):
 def print_name():
     print(__name__)
 
+# python test_module.py 와 같이 실행할 경우에는 아래 코드가 실행됨 
+if __name__ == '__main__':
+    print('원의 반지름 10')
+    print(f' 원 둘레 {get_circumference(10):.4f}, 원 넓이 {get_area(10):.4f}')
+
